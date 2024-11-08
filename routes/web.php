4bdc2f48bfcdf4a4a5a92eb65c4de681d0215fd0
3ggie% <151058698+3ggie-AB@users.noreply.github.com>
2024-11-08 18:03:07 +0700

    Ngaplod API

--- routes/web.php
+++ routes/web.php
@@ -6,7 +6,7 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
-
+Route::apiResource('/posts', App\Http\Controllers\Api\PostController::class);
 Route::get('/dashboard', function () {
     return view('dashboard');
 })->middleware(['auth', 'verified'])->name('dashboard');
